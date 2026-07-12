24d1828448f855abbf5bdbe9a53ba78620ebc5e7
buyonumainfo-hub <buyonumainfo@gmail.com>
2026-07-13 00:57:20 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import compression from 'compression';
 import morgan from 'morgan';
 import hpp from 'hpp';
 
-//import authRoutes         from './routes/auth.js';
+import authRoutes         from './routes/auth.js';
 import sellerAuthRoutes   from './routes/sellerAuth.js';
 import sellerRoutes       from './routes/sellers.js';
 import productRoutes      from './routes/products.js';
